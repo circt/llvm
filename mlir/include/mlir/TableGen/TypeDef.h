8f7551f5637cea56e4171cce1d212030c60da90a
John Demme <john.demme@microsoft.com>
2020-10-01 18:18:38 +0000

    Cleanup tblgen-typedef code based on @lattner's comments

--- mlir/include/mlir/TableGen/TypeDef.h
+++ mlir/include/mlir/TableGen/TypeDef.h
@@ -1,3 +1,4 @@
+//===-- TypeDefs.td - Record wrapper for type definitions --*- tablegen -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- mlir/test/lib/Dialect/Test/TestTypes.cpp
+++ mlir/test/lib/Dialect/Test/TestTypes.cpp
@@ -1,4 +1,4 @@
-//===- TestTypes.cpp - MLIR Test Dialect Types ----------------*- C++ -*-===//
+//===- TestTypes.cpp - MLIR Test Dialect Types ------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
